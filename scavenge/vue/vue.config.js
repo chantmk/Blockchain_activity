ad473a2dc14d66836bd68801c78c848dda528021
ThammakornK <chan_tham@hotmail.com>
2020-10-19 17:01:57 +0700

    push scavenge activity

---
+++ scavenge/vue/vue.config.js
@@ -0,0 +1,8 @@
+// module.exports = { };
+// vue.config.js
+module.exports = {
+    // options...
+    devServer: {
+        disableHostCheck: true
+    }
+}
---
+++ scavenge/x/scavenge/types/querier.go
@@ -0,0 +1,18 @@
+// package types
+
+
+// const (QueryListScavenge = "list-scavenge")
+		
+// const (QueryListCommit = "list-commit")
+		
+package types
+
+const (
+	QueryListScavenge = "list-scavenge"
+	QueryGetScavenge  = "get-scavenge"
+)
+
+const (
+	QueryListCommit = "list-commit"
+	QueryGetCommit  = "get-commit"
+)
